a2ca2c42a1f47a7005f96a990191d644e9236b1e
BaMarkham1 <benjamin.lemarc@ucdenver.edu>
2020-04-16 20:22:56 -0600

    flask commit 2

--- app.py
+++ app.py
@@ -21,15 +21,15 @@ driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), c
 
 app = Flask(__name__)
 
-@app.route('/advanced/<string:category>/<int:year>', methods=['GET'])
+@app.route('/advanced/<string:category>/<int:season>', methods=['GET'])
 def get_rushing(category, season):
     print("received get request")
-    #url = "https://www.pro-football-reference.com/years/2019/rushing_advanced.htm"
+    url = "https://www.pro-football-reference.com/years/" + season + "/" + category + "_advanced.htm"
     #load the page
-    #driver.get(url)
-    #print("got content")
-    rushing_info = {"category" : category, "season" : season}
-    #rushing_info = { "html" : str(driver.page_source)}
+    driver.get(url)
+    print("got content")
+    rushing_info = {}
+    rushing_info = {"category" : category, "season" : season, "html" : str(driver.page_source)}
     print("returning content")
     return json.dumps(rushing_info)
 
